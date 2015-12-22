6fd3cb9845ec8033731f54e4db8eaf984e2f052b
Yovin Lekamge <Yovin Lekamge>
2015-12-23 01:40:39 +0530

    added

---
+++ CampelloInvoice/Framework/ext-modules/psFramework/psFrameworkDirective.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+angular.module("psFramework").directive("psFramework", function () {
+    return {
+        transclude: false,
+        scope: {
+
+        },
+        controller: "psFrameworkController",
+        templateUrl: "ext-modules/psFramework/psFrameworkTemplate.html"
+        
+    };
+});
